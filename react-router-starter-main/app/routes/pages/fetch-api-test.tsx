80a904079eceab9724e9192b313c79f9c02baf2c
Hwang <hgm1557@naver.com>
2025-06-19 12:31:19 +0900

    get 테스트

---
+++ react-router-starter-main/app/routes/pages/fetch-api-test.tsx
@@ -0,0 +1,12 @@
+//https://jsonplaceholder.typicode.com/comments
+// 위 api를 이용하여 데이터를 불러와서 화면에 띄어주세요.
+
+const FetchApiTest = () => {
+  return (
+    <div>
+      <h1>전체 댓글 목록</h1>
+    </div>
+  );
+};
+
+export default FetchApiTest;
