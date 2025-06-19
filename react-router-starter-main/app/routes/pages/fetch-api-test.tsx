3a33d8f078f121ed394e24d0b9488c677fb4dac7
Hwang <hgm1557@naver.com>
2025-06-19 12:32:13 +0900

    문제설명 추가

--- react-router-starter-main/app/routes/pages/fetch-api-test.tsx
+++ react-router-starter-main/app/routes/pages/fetch-api-test.tsx
@@ -1,5 +1,5 @@
 //https://jsonplaceholder.typicode.com/comments
-// 위 api를 이용하여 데이터를 불러와서 화면에 띄어주세요.
+// 위 api를 이용하여 데이터를 불러와서 name, email, body를 화면에 띄어주세요
 
 const FetchApiTest = () => {
   return (
